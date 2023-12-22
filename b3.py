6df18625fca82c09c825e131a4ade1766e908ae5
Matheus Henrique Bertuci <mateusbertuci@hotmail.com>
2023-12-22 17:12:23 -0300

    Update b3.py

--- b3.py
+++ b3.py
@@ -8,58 +8,94 @@ def analyze_stock_performance(ticker, start_date, end_date, opening_drop_range):
     performance_list = []
 
     for opening_drop_percentage in np.arange(opening_drop_range[0], opening_drop_range[1], 0.1):
-        try:
-            # Carrega dados históricos da ação entre as datas especificadas
-            stock_data = yf.download(ticker, start=start_date, end=end_date)
+        # Carrega dados históricos da ação entre as datas especificadas
+        stock_data = yf.download(ticker, start=start_date, end=end_date)
 
-            # Verifica se há dados suficientes para análise
-            if stock_data.empty:
-                continue
+        # Calcula o preço de abertura ajustado pela porcentagem de queda
+        stock_data['Adjusted Opening'] = stock_data['Close'].shift(1) * (1 - opening_drop_percentage / 100)
 
-            # Calcula o preço de abertura ajustado pela porcentagem de queda
-            stock_data['Adjusted Opening'] = stock_data['Close'].shift(1) * (1 - opening_drop_percentage / 100)
+        # Calcula métricas
+        higher_count = (stock_data['Close'] > stock_data['Adjusted Opening']).sum()
+        lower_count = (stock_data['Close'] < stock_data['Adjusted Opening']).sum()
+        positive_differences = stock_data[stock_data['Close'] > stock_data['Adjusted Opening']]['Close'] / stock_data['Adjusted Opening'] - 1
+        average_positive_difference = positive_differences.mean() * 100  # Convertendo para porcentagem
+        total_days = len(stock_data)
+        higher_percentage = (higher_count / total_days) * 100 if total_days > 0 else 0
+        lower_percentage = (lower_count / total_days) * 100 if total_days > 0 else 0
 
-            # Calcula métricas
-            higher_count = (stock_data['Close'] > stock_data['Adjusted Opening']).sum()
-            lower_count = (stock_data['Close'] < stock_data['Adjusted Opening']).sum()
-            positive_differences = stock_data[stock_data['Close'] > stock_data['Adjusted Opening']]['Close'] / stock_data['Adjusted Opening'] - 1
-            average_positive_difference = positive_differences.mean() * 100  # Convertendo para porcentagem
-            total_days = len(stock_data)
-            higher_percentage = (higher_count / total_days) * 100 if total_days > 0 else 0
-            lower_percentage = (lower_count / total_days) * 100 if total_days > 0 else 0
-
-            # Adiciona os resultados à lista
-            performance_list.append({
-                'Ticker': ticker,
-                'Drop Percentage Range': f"{opening_drop_percentage:.1f}%",
-                'Higher Count': higher_count,
-                'Lower Count': lower_count,
-                'Higher Percentage': higher_percentage,
-                'Lower Percentage': lower_percentage,
-                'Average Positive Difference': average_positive_difference
-            })
-        except Exception as e:
-            st.write(f"Erro ao processar {ticker}: {e}")
+        # Adiciona os resultados à lista
+        performance_list.append({
+            'Ticker': ticker,
+            'Drop Percentage Range': f"{opening_drop_percentage:.1f}%",
+            'Higher Count': higher_count,
+            'Lower Count': lower_count,
+            'Higher Percentage': higher_percentage,
+            'Lower Percentage': lower_percentage,
+            'Average Positive Difference': average_positive_difference
+        })
 
     return performance_list
 
-# Lista de tickers (mantenha a sua lista original)
-tickers = ["WEGE3", "ITSA4", "ITSA3"] # Sua lista de tickers
-
+# Lista de tickers
+tickers = [
+    "MGLU3", "HAPV3", "AMER3", "ABEV3", "PETR4", "BBDC4", "B3SA3", "RAIZ4", "ITUB4", "PETZ3", "VALE3", "CIEL3",
+    "ITSA4", "CMIG4", "SEQL3", "COGN3", "EMBR3", "CPLE6", "AZUL4", "SOMA3", "RAIL3", "EQTL3", "GGBR4", "GOAU4",
+    "CVCB3", "VBBR3", "LREN3", "PETR3", "CRFB3", "BBDC3", "JBSS3", "PDGR3", "BRFS3", "BEEF3", "GOLL4", "BRKM5",
+    "CCRO3", "BBAS3", "MRFG3", "PCAR3", "MRVE3", "USIM5", "IFCM3", "TRPL4", "CSNA3", "AERI3", "BBSE3", "ANIM3",
+    "CMIN3", "STBP3", "CPLE3", "TIMS3", "RENT3", "ENEV3", "RDOR3", "ASAI3", "UGPA3", "ELET3", "CSAN3", "WEGE3",
+    "NTCO3", "AURE3", "POMO4", "GMAT3", "CEAB3", "PRIO3", "DXCO3", "RADL3", "GFSA3", "RRRP3", "SUZB3", "SLCE3",
+    "QUAL3", "LWSA3", "YDUQ3", "VAMO3", "BRAP4", "BMGB4", "EGIE3", "OIBR3", "CYRE3", "CBAV3", "SBSP3", "KLBN4",
+    "BPAN4", "BRSR6", "ALPA4", "AZEV4", "ESPA3", "GGPS3", "SMFT3", "TTEN3", "ONCO3", "ZAMP3", "CXSE3", "SAPR4",
+    "RECV3", "HBSA3", "GUAR3", "RCSL4", "ENAT3", "TEND3", "LJQQ3", "PLPL3", "TOTS3", "MTRE3", "SBFG3", "MULT3",
+    "CSMG3", "RCSL3", "ECOR3", "RAPT4", "CASH3", "FLRY3", "CURY3", "SIMH3", "MOVI3", "DIRR3", "IRBR3", "HBRE3",
+    "HYPE3", "SMTO3", "CPFE3", "ARZZ3", "AESB3", "JHSF3", "EZTC3", "VIVT3", "LIGT3", "MLAS3", "PSSA3", "KLBN3",
+    "MYPK3", "EVEN3", "INTB3", "VIVA3", "ABCB4", "ELET6", "VVEO3", "AMBP3", "SEER3", "MBLY3", "PGMN3", "MDIA3",
+    "TRIS3", "HBOR3", "KEPL3", "AZEV3", "JALL3", "ODPV3", "RANI3", "NEOE3", "ORVR3", "MEAL3", "GRND3", "CAML3",
+    "AALR3", "POSI3", "BRIT3", "WIZC3", "SGPS3", "MDNE3", "MILS3", "VULC3", "WEST3", "AGRO3", "LUPA3", "NGRD3",
+    "PINE4", "CMIG3", "FRAS3", "DESK3", "SHUL4", "ROMI3", "AMAR3", "BLAU3", "LEVE3", "TASA4", "KRSA3", "PNVL3",
+    "PTBL3", "USIM3", "OPCT3", "CSED3", "SAPR3", "ITSA3", "TRAD3", "ALPK3", "MATD3", "CLSA3", "BMOB3", "LAVV3",
+    "ITUB3", "VLID3", "TUPY3", "MELK3", "ETER3", "CTNM4", "ENJU3", "IGTI3", "IGTI3", "TGMA3", "UNIP6", "SOJA3",
+    "FIQE3", "RNEW3", "VITT3", "VIVR3", "PRNR3", "ALLD3", "ARML3", "TAEE4", "TCSA3", "SHOW3", "RNEW4", "JSLG3",
+    "DASA3", "GGBR3", "PORT3", "DMVF3", "SYNE3", "PFRM3", "CAMB3", "TAEE3", "NINJ3", "FESA4", "TFCO4", "SANB3",
+    "CTSA3", "TECN3", "CSUD3", "SANB4", "RSID3", "LOGG3", "GOAU3", "INEP3", "ELMD3", "CTSA4", "ATMP3", "EQPA3",
+    "JFEN3", "EUCA4", "PDTC3", "BIOM3", "VSTE3", "TEKA4", "POMO3", "LOGN3", "BRKM3", "LPSB3", "LVTC3", "LAND3",
+    "DEXP3", "SNSY5", "DOTZ3", "INEP4", "UCAS3", "BOBR4", "BRAP3", "CEBR5", "AGXY3", "TASA3", "DOHL4", "RAPT3",
+    "OIBR4", "OSXB3", "CEBR6", "SCAR3", "CLSC4", "CGRA4", "ALUP4", "TPIS3", "COCE5", "HAGA4", "WHRL4", "UNIP3",
+    "BEES3", "ALUP3", "RDNI3", "HOOT4", "PMAM3", "ATOM3", "WHRL3", "PTNT4", "FHER3", "CEBR3", "REDE3", "RPMG3",
+    "OFSA3", "CEDO4", "APER3", "BPAC3", "BEES4", "TRPL3", "ENGI4", "BRSR3", "CGRA3", "BMEB4", "BAZA3", "MTSA4",
+    "DEXP4", "FRTA3", "EMAE4", "AVLL3", "EALT4", "RSUL4", "BPAC5", "FRIO3", "EPAR3", "NUTR3", "CRPG5", "MNPR3",
+    "ALPA3", "CGAS5", "BGIP4", "GEPA4", "NEXP3", "BRIV4", "ENGI3", "BAHI3", "BALM4", "WLMM4", "CSRN3", "BSLI3",
+    "CLSC3", "HETA4", "EQMA3B", "EUCA3", "TELB4", "ESTR4", "CTNM3", "AFLT3", "SNSY3", "MNDL3", "CEEB3", "CEED4",
+    "BSLI4", "EALT3", "PLAS3", "BMIN3", "CTKA4", "BMIN4", "PTNT3", "RPAD5", "FESA3", "EKTR4", "CRPG6", "CSRN6",
+    "MWET4", "HAGA3", "BMKS3", "ENMT3", "IGTI4", "IGTI4", "BALM3", "FIEI3", "MRSA3B", "RPAD6", "CALI3", "BMEB3",
+    "TELB3", "BRGE12", "NORD3", "CGAS3", "BNBR3", "CPLE5", "MGEL4", "BRSR5", "BRGE11", "DTCY3", "CEED3", "BAUH4",
+    "BDLL4", "CRIV4", "UNIP5", "PATI3", "CEDO3", "MRSA5B", "LIPR3", "CSAB4", "PEAB3", "LUXM4", "BRKM6", "BDLL3",
+    "BRGE8", "RPAD3", "CRIV3", "BRGE6", "ENMT4", "MERC4", "GEPA3", "CTKA3", "BRIV3", "JOPA3", "WLMM3", "AHEB3",
+    "DOHL3", "CBEE3", "BRGE3", "MAPT4", "GSHP3", "CSAB3", "MOAR3", "EQPA7", "CRPG3", "MEGA3", "EQPA5", "HBTS5",
+    "BGIP3", "DMFN3", "CEEB5", "CSRN5", "BRGE5", "TKNO4", "ELET5", "SOND6", "SOND5", "GPAR3", "SQIA3", "ESTR3",
+    "BRGE7", "MRSA6B", "ALSO3", "BRPR3", "PATI4", "MTSA3", "SLED4", "SLED3", "EQPA6", "AHEB6", "PINE3", "VIIA3",
+    "EKTR3", "MWET3", "USIM6", "AHEB5", "ENBR3", "BOAS3", "PEAB4", "COCE3", "JOPA4", "MODL3", "MERC3", "CEGR3",
+    "MAPT3", "CRDE3", "IGBR3", "MSPA4", "ODER4", "PARD3", "CASN3", "WIZS3", "LLIS3", "MSPA3", "BRML3", "DMMO3",
+    "GETT3", "GETT4", "SULA4", "SULA3", "CEPE5", "TCNO4", "TCNO3", "CEPE6", "BKBR3", "MTIG4", "BLUT4", "BLUT3",
+    "MODL4", "CARD3", "SHUL3", "FIGE3", "FNCN3", "TEKA3", "HETA3", "LCAM3", "BIDI4", "BIDI3", "EEEL4", "EEEL3",
+    "BBRK3", "SOND3", "CESP6", "CESP3", "CESP5", "ECPR4", "MOSI3", "POWE3", "ECPR3", "GNDI3", "LAME4", "LAME3",
+    "OMGE3", "IGTA3", "JPSA3", "BRDT3", "JBDU4", "JBDU3", "HGTX3", "CCPR3", "DTEX3", "VVAR3", "PNVL4", "TESA3",
+    "BTOW3", "LINX3", "BTTL3", "GPCP3", "GPCP4", "SMLS3", "MMXM3", "BSEV3", "CNTO3", "TIET4", "TIET3", "CORR4",
+    "CEPE3", "CALI4", "SNSY6", "CASN4", "EMAE3", "BPAR3", "APTI4", "VSPT3", "MTIG3", "FIGE4", "LUXM3", "TKNO3",
+    "COCE6", "MGEL3", "CTSA8", "MMAQ4"
+]
 # Adiciona o sufixo '.SA' necessário para o yfinance
 tickers_b3 = [ticker + ".SA" for ticker in tickers]
 
-# Título do aplicativo
+# Interface do Streamlit
 st.title("Análise de Desempenho de Ações")
 
-# Seletores de entrada para o usuário definir as variáveis
 start_date = st.date_input("Data de Início", value=pd.to_datetime("2023-12-01"))
 end_date = st.date_input("Data de Fim", value=pd.to_datetime("2023-12-21"))
 opening_drop_start = st.number_input("Queda Inicial (%)", min_value=0.1, max_value=100.0, value=0.10, step=0.1)
 opening_drop_end = st.number_input("Queda Final (%)", min_value=0.1, max_value=100.0, value=0.50, step=0.1)
 opening_drop_range = (opening_drop_start, opening_drop_end)
 
-# Botão para realizar a análise
 if st.button("Analisar"):
     final_performance_results = []
     for ticker in tickers_b3:
@@ -67,13 +103,8 @@ if st.button("Analisar"):
         final_performance_results.extend(ticker_performance)
 
     performance_df = pd.DataFrame(final_performance_results)
+    performance_df.sort_values(by=['Ticker', 'Average Positive Difference'], ascending=[True, False], inplace=True)
+    
+    st.dataframe(performance_df)
 
-    # Verifica se as colunas necessárias estão no DataFrame
-    if 'Ticker' in performance_df.columns and 'Average Positive Difference' in performance_df.columns:
-        performance_df.sort_values(by=['Ticker', 'Average Positive Difference'], ascending=[True, False], inplace=True)
-        st.dataframe(performance_df)
-    else:
-        st.write("Colunas necessárias não encontradas no DataFrame")
-
-# Rodapé
 st.write("Desenvolvido por [Seu Nome ou Organização]")
