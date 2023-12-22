884cf5381745e37a9b1669e701cf83f623c19471
Matheus Henrique Bertuci <mateusbertuci@hotmail.com>
2023-12-22 17:04:31 -0300

    Update b3.py

--- b3.py
+++ b3.py
@@ -3,29 +3,31 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 
-# Função para analisar o desempenho das ações
+# Função para análise do desempenho das ações
 def analyze_stock_performance(ticker, start_date, end_date, opening_drop_range):
     performance_list = []
 
-    if start_date >= end_date:
-        st.error('A data inicial deve ser anterior à data final.')
-        return performance_list
-
     for opening_drop_percentage in np.arange(opening_drop_range[0], opening_drop_range[1], 0.1):
+        # Carrega dados históricos da ação entre as datas especificadas
         stock_data = yf.download(ticker, start=start_date, end=end_date)
 
+        # Verifica se há dados suficientes para análise
         if stock_data.empty:
             continue
 
+        # Calcula o preço de abertura ajustado pela porcentagem de queda
         stock_data['Adjusted Opening'] = stock_data['Close'].shift(1) * (1 - opening_drop_percentage / 100)
+
+        # Calcula métricas
         higher_count = (stock_data['Close'] > stock_data['Adjusted Opening']).sum()
         lower_count = (stock_data['Close'] < stock_data['Adjusted Opening']).sum()
         positive_differences = stock_data[stock_data['Close'] > stock_data['Adjusted Opening']]['Close'] / stock_data['Adjusted Opening'] - 1
-        average_positive_difference = positive_differences.mean() * 100
+        average_positive_difference = positive_differences.mean() * 100  # Convertendo para porcentagem
         total_days = len(stock_data)
         higher_percentage = (higher_count / total_days) * 100 if total_days > 0 else 0
         lower_percentage = (lower_count / total_days) * 100 if total_days > 0 else 0
 
+        # Adiciona os resultados à lista
         performance_list.append({
             'Ticker': ticker,
             'Drop Percentage Range': f"{opening_drop_percentage:.1f}%",
@@ -38,38 +40,34 @@ def analyze_stock_performance(ticker, start_date, end_date, opening_drop_range):
 
     return performance_list
 
-# Definindo a interface do usuário no Streamlit
-def main():
-    st.title("Análise de Desempenho de Ações")
+# Lista de tickers (mantenha a sua lista original)
+tickers = ["WEGE3", "ITSA4"] # Sua lista de tickers
 
-    start_date = st.date_input("Data inicial", value=pd.to_datetime("2022-01-01"))
-    end_date = st.date_input("Data final", value=pd.to_datetime("2023-01-01"))
-    min_drop = st.number_input("Porcentagem de queda mínima", min_value=0.0, max_value=100.0, value=0.10)
-    max_drop = st.number_input("Porcentagem de queda máxima", min_value=0.0, max_value=100.0, value=0.50)
+# Adiciona o sufixo '.SA' necessário para o yfinance
+tickers_b3 = [ticker + ".SA" for ticker in tickers]
 
-    if st.button("Analisar Desempenho"):
-        tickers = ["WEGE3", "ITSA4"]
-        tickers_b3 = [ticker + ".SA" for ticker in tickers]
-        final_performance_results = []
+# Título do aplicativo
+st.title("Análise de Desempenho de Ações")
 
-        for ticker in tickers_b3:
-            ticker_performance = analyze_stock_performance(ticker, start_date, end_date, (min_drop, max_drop))
-            final_performance_results.extend(ticker_performance)
+# Seletores de entrada para o usuário definir as variáveis
+start_date = st.date_input("Data de Início", value=pd.to_datetime("2023-12-01"))
+end_date = st.date_input("Data de Fim", value=pd.to_datetime("2023-12-21"))
+opening_drop_start = st.number_input("Queda Inicial (%)", min_value=0.1, max_value=100.0, value=0.10, step=0.1)
+opening_drop_end = st.number_input("Queda Final (%)", min_value=0.1, max_value=100.0, value=0.50, step=0.1)
+opening_drop_range = (opening_drop_start, opening_drop_end)
 
-        if not final_performance_results:
-            st.error("Não foram encontrados dados para os critérios selecionados.")
-            return
+# Botão para realizar a análise
+if st.button("Analisar"):
+    final_performance_results = []
+    for ticker in tickers_b3:
+        ticker_performance = analyze_stock_performance(ticker, start_date, end_date, opening_drop_range)
+        final_performance_results.extend(ticker_performance)
 
-        performance_df = pd.DataFrame(final_performance_results)
+    performance_df = pd.DataFrame(final_performance_results)
+    performance_df.sort_values(by=['Ticker', 'Average Positive Difference'], ascending=[True, False], inplace=True)
 
-        if performance_df.empty:
-            st.error("Nenhum resultado foi retornado. Por favor, verifique os critérios de pesquisa.")
-        else:
-            try:
-                performance_df.sort_values(by=['Ticker', 'Average Positive Difference'], ascending=[True, False], inplace=True)
-                st.dataframe(performance_df)
-            except KeyError as e:
-                st.error(f"Erro ao ordenar o DataFrame: {e}")
+    # Mostra o DataFrame no Streamlit
+    st.dataframe(performance_df)
 
-if __name__ == "__main__":
-    main()
+# Rodapé
+st.write("Desenvolvido por [Seu Nome ou Organização]")
